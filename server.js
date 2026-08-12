160fee2486ac880a7e05e69403aba99274486226
gemmaster69 <gemmaster6988@gmail.com>
2026-08-12 11:54:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'NIM proxy is running' });
